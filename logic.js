c7dd72d0fa67e6a6cdb683366d9f5d3614282786
axelsundin <axelsundin@hotmail.com>
2021-03-23 13:33:21 +0100

    game logic

--- logic.js
+++ logic.js
@@ -1,44 +1,100 @@
-var randomNumber = Math.random()*20;
-randomNumber = Math.ceil(randomNumber);
+let randomNumber = Math.random()*20
+randomNumber = Math.ceil(randomNumber)
 
-    console.log(randomNumber);
+console.log(randomNumber)
+
+function awaitNoob() {
+    console.log("awaitNoob running")
+    setTimeout(noobBotGuess, 3000)  
+}
+
+function awaitInstructPlayer() {
+    console.log("awaitInstructPlayer running")
+    setTimeout(playerNext, 3000)
+}
+
+function awaitInstructNoob() {
+    console.log("awaitInstructNoob running")
+    setTimeout(noobNext, 3000)
+}
+
+function noobNext() {
+    let instruct = document.getElementById("timerDiv")
+    instruct.innerHTML = ""
+    instruct.innerHTML = "Noob, you're up!"
+    awaitNoob()
+}
+
+function playerNext() {
+    let instruct = document.getElementById("timerDiv")
+    instruct.innerHTML = ""
+    instruct.innerHTML = "Player, you're up!"
+}
 
 function guess() {
+    let instruct = document.getElementById("timerDiv")
     let guess = document.getElementById("getNumber").value
-    if (guess < randomNumber){
-    let instruct = document.getElementById("instruct")
-    instruct.innerHTML = "";
-    instruct.innerHTML = "Higher";
-    noobBotGuess()
+    let you = document.getElementById("you")
 
+    you.innerHTML = ""
+    you.innerHTML = guess
+    
+    if (guess < randomNumber){
+        instruct.innerHTML = ""
+        instruct.innerHTML = "Higher"
+        awaitInstructNoob()
+    
     } else if (guess > randomNumber) {
-        let instruct = document.getElementById("instruct")
-        instruct.innerHTML = "";
+        instruct.innerHTML = ""
         instruct.innerHTML = "Lower"
-        noobBotGuess();
+        awaitInstructNoob()
+
     } else if (guess == randomNumber) {
-        instruct.innerHTML = "";
-        instruct.innerHTML = "Great you win! Thats my number!";
+        instruct.innerHTML = ""
+        instruct.innerHTML = "Great you win! Thats my number!"
+        
+        if (confirm('Great you win! Thats my number! Do you want to play again?')) {
+            // Yes
+            window.location.reload()
+          } else {
+            window.location("index.html")
+          }
         }
     }
 
     function noobBotGuess(){
-        var noob = Math.random()*20;
-        noob = Math.ceil(noob);
-        console.log(noob)
-        if (noob < randomNumber){
-            let instruct = document.getElementById("instruct")
-            instruct.innerHTML = "";
-            instruct.innerHTML = "Higher";
+        let instruct = document.getElementById("timerDiv")
+
+        let noobGuess = Math.random()*20
+        noobGuess = Math.ceil(noobGuess)
+        console.log(noobGuess)
+
+        let noob = document.getElementById("noob")
+        noob.innerHTML = ""
+        noob.innerHTML = noobGuess
+
+        if (noobGuess < randomNumber){
+            
+            instruct.innerHTML = ""
+            instruct.innerHTML = "Higher"
+            awaitInstructPlayer()
     
         
-            } else if (noob > randomNumber) {
-                let instruct = document.getElementById("instruct")
-                instruct.innerHTML = "";
-                instruct.innerHTML = "Lower";
-            } else if (noob == randomNumber) {
-                instruct.innerHTML = "";
-                instruct.innerHTML = "Noob bot wins! Thats my number!";
+        } else if (noobGuess > randomNumber) {
+            instruct.innerHTML = ""
+            instruct.innerHTML = "Lower"
+            awaitInstructPlayer()
+
+        } else if (noobGuess == randomNumber) {
+            instruct.innerHTML = ""
+            instruct.innerHTML = "Noob bot wins! Thats my number!"
+            
+            if (confirm('Noob bot wins! Thats my number! Do you want to play again?')) {
+                // Yes
+                window.location.reload()
+                } else {
+                // No
+                }
             }
     }
 
