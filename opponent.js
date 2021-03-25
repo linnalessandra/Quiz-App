7cee892c05c0eee6e9c417597ed718be54c16708
axelsundin <axelsundin@hotmail.com>
2021-03-25 16:15:41 +0100

    update

--- opponent.js
+++ opponent.js
@@ -1,11 +1,16 @@
+localStorage.setItem("id", "Check1");
+
 function selectOnlyThis(id) {
 
     for (var i = 1;i <= 3; i++) {
         document.getElementById("Check" + i).checked = false;
     }
     document.getElementById(id).checked = true;
+    localStorage.setItem("id", id);
 }
 
 function playGame() {
         window.location.href = "game.html"
 }
+
+
